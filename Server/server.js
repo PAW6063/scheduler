44c82d9b618ff65396d8153c62a75247ffc17856
Phillip <phillip.welch2@mavs.uta.edu>
2022-06-02 13:32:02 -0500

    File Structure
    File structure and basic server setup

--- Server/server.js
+++ Server/server.js
@@ -1,20 +1,7 @@
-const express = require('express');
-const path = require('path');
-
+const express = require("express");
 const app = express();
-
 const PORT = 3000;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-//app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-    res.send('Hello');
-    //res.sendFile(__dirnamec + 'index.js');
-});
-
 app.listen(PORT, () => {
-    console.log(`Application listening on port ${PORT}...`);
-})
+  console.log(`Application listening on port ${PORT}...`);
+});
